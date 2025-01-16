901138d3a5b27dc52e9b523d3007212495fbf06d
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-01-16 13:57:41 +0500

    Day 1 Completed.Learn about numbers,strings and alert and console.log

---
+++ Day_1/Intro.js
@@ -0,0 +1,6 @@
+// What is Javascript?
+// 1. Giving Instruction To Computer.
+// 2. Computer Follows Our Instruction to do task.
+
+alert('Hello World') // used for displaying a pop on web page.
+console.log('Hello World!')  // used to display data on the terminal.
---
+++ Day_1/Strings.js
@@ -0,0 +1,25 @@
+// ******************** String ********************
+
+// String is used for working with text like hello world.
+// String can be used inside a single qoute or double quote.
+
+alert('Hello')
+alert("Hello")
+
+// Both these will treated as string.
+
+
+// This is string concatenation which means adding the stringd.
+console.log('Hello' + ' world')
+
+
+// Check type of variable using typeof function
+
+const Num = 2;
+typeof(Num)
+
+// This will tell the type of the num variable
+
+
+// For Multiline strings we use backtick
+//   `hello`
