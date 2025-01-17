128215533bd1550420796cadf18c0c76741184fe
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-01-17 12:28:43 +0500

    Day 1 intro

--- Day_1/Intro.js
+++ Day_1/Intro.js
@@ -2,5 +2,5 @@
 // 1. Giving Instruction To Computer.
 // 2. Computer Follows Our Instruction to do task.
 
-alert('Hello World') // used for displaying a pop on web page.
+// alert('Hello World') // used for displaying a pop on web page.
 console.log('Hello World!')  // used to display data on the terminal.
